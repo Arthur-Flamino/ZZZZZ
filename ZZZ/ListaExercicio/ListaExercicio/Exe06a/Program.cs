76a99d0e7e02ab51302d0ef90406eefea9323879
ARTHUR FELIPE FLAMINO DE OLIVEIRA <arthur.oliveira@cs.up.edu.br>
2025-03-13 10:16:32 -0300

    zzzzz

---
+++ ZZZ/ListaExercicio/ListaExercicio/Exe06a/Program.cs
@@ -0,0 +1,67 @@
+﻿//PASSOS PARA RESOLVER O EXERCÍCIO
+//1 - Criar um vetor de 1000 posições
+//2 - Preencher o vetor com valores aleatórios
+//3 - Imprimir o vetor na tela
+//4 - Ordenar o vetor com o Bubble Sort
+
+//Criar um vetor de valores inteiros
+int[] vetor = new int[100];
+
+//Gerar um valor randômico
+Random r = new Random();
+
+//Preencher o vetor com valores aleatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(1000);
+}
+
+//Imprimir o vetor com valores aletatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+//Ordenar o vetor com o Bubble Sort
+
+// for (int j = 0; j < vetor.Length; j++)
+// {
+//     for (int i = 0; i < vetor.Length - 1 -j; i++)
+//     {
+//         //Trocar valores
+//         if (vetor[i] > vetor[i + 1])
+//         {
+//             int aux = vetor[i];
+//             vetor[i] = vetor[i + 1];
+//             vetor[i + 1] = aux;
+//         }
+//     }
+    
+// }
+
+bool troca = false;
+do
+{
+    for (int i = 0; i < vetor.Length - 1; i++)
+        {
+            //Trocar valores
+            if (vetor[i] > vetor[i + 1])
+            {
+                int aux = vetor[i];
+                vetor[i] = vetor[i + 1];
+                vetor[i + 1] = aux;
+                troca = true;
+            }else
+            {
+                troca = false;
+            }
+        }
+    
+} while (troca);
+
+Console.WriteLine("\n");
+//Imprimir o vetor com valores ordenados
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
---
+++ ZZZ/ListaExercicio/ListaExercicio/Exe06b/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+//Criar um vetor de valores inteiros
+int[] vetor = new int[100];
+
+//Gerar um valor randômico
+Random r = new Random();
+
+//Preencher o vetor com valores aleatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(1000);
+}
+
+//Imprimir o vetor com valores aletatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+//Ordenar o vetor com o Bubble Sort
+
+Array.Sort(vetor);
+
+Console.WriteLine("\n");
+//Imprimir o vetor com valores ordenados
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
